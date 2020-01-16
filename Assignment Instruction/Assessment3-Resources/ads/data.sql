f76995e011ce3e2836a0bb770cec7a777770ac46
Dewei Liu <deweiliu@hotmail.com>
2020-01-16 14:36:45 +0000

    update

--- Assignment Instruction/Assessment3-Resources/ads/data.sql
+++
@@ -1,4 +0,0 @@
-INSERT INTO `advert` (`advertid`, `keyword`, `advert`) VALUES
-(1, 'network', 'Bob\'s networks are the best networks in all the world. Buy Bob\'s for the best networks!'),
-(2, 'monitoring', 'Network monitoring is difficult. Why not let the ropiest network monitor do it all for you, well at least until it fails and itself catches fire. FreeNATS - network monitoring you definitely can\'t trust.');
-
